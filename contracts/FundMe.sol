dc07df6030971cd27c69ae205886849b95f34ce5
Hijacked-Bunny <lucas.hannig@gmail.com>
2021-11-22 12:26:48 -0800

    first commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,75 @@
+//// SPDX-License-Identifier: MIT-3.0
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public Funders;
+    address public owner;
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        uint256 minimumUsd = 5 * 10**18;
+        require(
+            getConversionRate(msg.value) >= minimumUsd,
+            "You need to pay at least $5"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+        Funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10000000000);
+    }
+
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
+ function getEntranceFee() public view returns (uint256) {
+        // mimimumUSD
+        uint256 mimimumUSD = 50 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (mimimumUSD * precision) / price;
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() public payable onlyOwner {
+        msg.sender.transfer(address(this).balance);
+        //require(msg.sender == owner);
+        for (
+            uint256 FunderIndex = 0;
+            FunderIndex < Funders.length;
+            FunderIndex++
+        ) {
+            address funder = Funders[FunderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        Funders = new address[](0);
+    }
+}
